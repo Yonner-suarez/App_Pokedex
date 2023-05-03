3c7b3a7663151716d8796f388652cd83988aecd7
Yonner-suarez <yonnerhazziel@gmail.com>
2023-05-02 21:58:37 -0500

    form

--- Client/src/components/SingUp/SingUp.jsx
+++ Client/src/components/SingUp/SingUp.jsx
@@ -1,18 +1,51 @@
+import { useState } from "react";
 import style from "./SingUp.module.css";
 
 const SingUp = () => {
+  const [user, setUser] = useState({
+    userName: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const onChange = (event) => {
+    const key = event.target.name,
+      value = event.target.value;
+
+    setUser({
+      ...user,
+      [key]: value,
+    });
+  };
   return (
     <div className={style.contenedor}>
       <div className={style.contform}>
         <form className={style.form}>
           <p>Create your account</p>
           <br />
-          <input type="text" placeholder="User Name" className={style.input} />
-          <br />
-          <input type="text" placeholder="Password" className={style.input} />
-          <br />
           <input
             type="text"
+            name="userName"
+            value={user.userName}
+            placeholder="User Name"
+            onChange={onChange}
+            className={style.input}
+          />
+          <br />
+          <input
+            type="password"
+            name="password"
+            value={user.password}
+            placeholder="Password"
+            onChange={onChange}
+            className={style.input}
+          />
+          <br />
+          <input
+            type="password"
+            name="confirmPassword"
+            value={user.confirmPassword}
+            onChange={onChange}
             placeholder="Confirm Password"
             className={style.input}
           />
