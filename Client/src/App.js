53da44f91b6493fd52167b36d023edc4c7bffd16
Yonner-suarez <yonnerhazziel@gmail.com>
2023-05-03 20:18:47 -0500

    front

--- Client/src/App.js
+++ Client/src/App.js
@@ -14,7 +14,7 @@ import axios from "axios";
 import { getUser } from "./Redux/action";
 import { useDispatch } from "react-redux";
 
-axios.defaults.baseURL = "https://localhost:3001";
+axios.defaults.baseURL = "https://apppokedex-production.up.railway.app/";
 //"https://apppokedex-production.up.railway.app/";
 
 function App() {
